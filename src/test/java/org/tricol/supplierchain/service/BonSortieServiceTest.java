58c8228e87af37e256bac9cfccda1726f6e171a5
abdellatif guezadi <abdellatifguezadi@gmail.com>
2025-11-11 16:48:49 +0100

    test: add unit test de consumation un seul lot

--- src/test/java/org/tricol/supplierchain/service/BonSortieServiceTest.java
+++ src/test/java/org/tricol/supplierchain/service/BonSortieServiceTest.java
@@ -1,10 +1,16 @@
 package org.tricol.supplierchain.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.tricol.supplierchain.dto.response.BonSortieResponseDTO;
 import org.tricol.supplierchain.entity.BonSortie;
 import org.tricol.supplierchain.entity.LigneBonSortie;
+import org.tricol.supplierchain.entity.LotStock;
 import org.tricol.supplierchain.entity.Produit;
 import org.tricol.supplierchain.enums.StatutBonSortie;
 import org.tricol.supplierchain.mapper.BonSortieMapper;
@@ -13,15 +19,24 @@ import org.tricol.supplierchain.repository.LotStockRepository;
 import org.tricol.supplierchain.repository.MouvementStockRepository;
 import org.tricol.supplierchain.repository.ProduitRepository;
 import org.tricol.supplierchain.service.inter.BonSortieService;
+import org.tricol.supplierchain.service.inter.GestionStockService;
+import org.tricol.supplierchain.service.BonSortieServiceImpl;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+import static org.tricol.supplierchain.enums.MotifBonSortie.PRODUCTION;
+
+
+@ExtendWith(MockitoExtension.class)
 public class BonSortieServiceTest {
 
     @InjectMocks
-    BonSortieService bonSortieService;
+    BonSortieServiceImpl bonSortieService;
 
     @Mock
     LotStockRepository lotStockRepository;
@@ -33,6 +48,8 @@ public class BonSortieServiceTest {
     MouvementStockRepository mouvementStockRepository;
     @Mock
     BonSortieMapper bonSortieMapper;
+    @Mock
+    GestionStockService gestionStockService;
 
     private Produit produit;
     private BonSortie bonSortie;
@@ -56,10 +73,38 @@ public class BonSortieServiceTest {
                 .numeroBon("BS001")
                 .statut(StatutBonSortie.BROUILLON)
                 .ligneBonSorties(List.of(ligne))
+                .motif(PRODUCTION)
                 .build();
 
 
     }
 
+
+
+    @Test
+    @DisplayName("consumation un seul lot")
+    public void testConsumationUnSeulLot() {
+        LotStock lotStock = LotStock.builder()
+                .id(1L)
+                .quantiteRestante(new BigDecimal("20"))
+                .prixUnitaireAchat(new BigDecimal("5"))
+                .dateEntree(LocalDateTime.now().minusDays(5))
+                .build();
+
+        when(lotStockRepository.findByProduitIdOrderByDateEntreeAsc(1L))
+                .thenReturn(List.of(lotStock));
+
+        when(bonSortieMapper.toResponseDTO(any())).thenReturn(new BonSortieResponseDTO());
+
+        BonSortieResponseDTO responseDTO = bonSortieService.performActualValidation(bonSortie);
+
+        assertThat(responseDTO).isNotNull();
+        verify(mouvementStockRepository, times(1)).save(any());
+        verify(lotStockRepository,times(1)).save(any());
+        verify(bonSortieRepository,times(1)).save(any());
+
+        assertThat(lotStock.getQuantiteRestante()).isEqualTo(new BigDecimal("10"));
+
+    }
 
 }
